0d5649b8a95f4b913a93e96022fce77946b26d5c
Horst Gutmann <zerok@zerokspot.com>
2020-05-30 21:10:54 +0200

    Use exec.LookPath to search for magick

--- pkg/resizer/imagemagick.go
+++ pkg/resizer/imagemagick.go
@@ -14,8 +14,12 @@ type ImageMagickResizer struct{}
 
 func (r *ImageMagickResizer) Resize(ctx context.Context, out string, in string, width int64, height int64) error {
 	logger := zerolog.Ctx(ctx)
+	bin, err := exec.LookPath("magick")
+	if err != nil {
+		return err
+	}
 	logger.Debug().Msgf("%s -> %s", in, out)
-	cmd := exec.CommandContext(ctx, "magick", in, "-resize", fmt.Sprintf("%dx%d", width, height), out)
+	cmd := exec.CommandContext(ctx, bin, in, "-resize", fmt.Sprintf("%dx%d", width, height), out)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	return cmd.Run()
